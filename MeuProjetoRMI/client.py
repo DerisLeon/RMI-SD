11f53d7610a717c085cfe96351b6bfbb0c9babde
Déris Araújo <79857414+DerisLeon@users.noreply.github.com>
2023-05-29 15:50:36 -0300

    Update client.py

--- MeuProjetoRMI/client.py
+++ MeuProjetoRMI/client.py
@@ -4,7 +4,12 @@ uri = "PYRO:obj_81ee0fcf8f5d4d158a3cd3279fec6496@192.168.1.114:9090"
 servico_remoto = Pyro4.Proxy(uri)
 
 saudacao = servico_remoto.saudacao()
-resultado = servico_remoto.somar(2, 3)
 
-print("Resposta do servidor:", saudacao)
-print("Resultado da soma:", resultado)
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+resultado = servico_remoto.calcular_bhaskara(a, b, c)
+
+print("Status do servidor:", saudacao)
+print("Resultado da fórmula de Bhaskara:", resultado)
